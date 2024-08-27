7711641fe4c5a8e2b856b97ebfb4c6a53616108f
Daniyar Kashkaraliev <daniyar.kashkaraliev@gmail.com>
2024-08-28 00:58:03 +0600

    add suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import CharacterList from '@/components/character/character-list';
+import { Suspense } from 'react';
 
 export default async function Home() {
   return (
     <div className="p-6">
       <h2 className="text-2xl font-bold mb-5">Персонажи</h2>
+      <Suspense>
       <CharacterList />
+      </Suspense>
     </div>
   );
 }
